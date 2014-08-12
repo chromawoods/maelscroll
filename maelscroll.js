c3949183dd955602cdf0cba4edd2129cd0b7453f
Andreas Larsson <andreas@chromawoods.com>
2014-08-12 11:31:38 +0200

    First.

---
+++ maelscroll.js
@@ -0,0 +1,30 @@
+﻿(function(window) {
+
+    var elems, i, l, scrollTimer, iterateElems;
+        
+    if (!document.querySelectorAll) { return false; }
+
+    elems = Array.prototype.slice.call(document.querySelectorAll('img, a, p, li, dt, dd, th, td, label, h1, h2, h3, h4, h5, h6, button, input, textarea, select'));
+    l = elems.length;
+    
+    iterateElems = function(fn) {
+        for (i = 0; i < l; i++) { fn(elems[i]); }
+    };
+    
+    iterateElems(function(el) {
+        el.style.transition = 'transform 1s ease-out';
+    });
+    
+    window.onscroll = function() {
+    
+        clearTimeout(scrollTimer);
+        
+        scrollTimer = setTimeout(function() {
+            iterateElems(function(el) {
+                el.style.transform = 'rotate(' + parseInt((window.pageYOffset / 360) *100, 10) + 'deg)';
+            });
+        }, 120);
+        
+    };
+
+}(this));
